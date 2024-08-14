726812df6af6b80d54a05ed7e9fbb1c5f28fcf34
JonathanKaiKira <jonathancurinan@gmail.com>
2024-08-13 23:47:40 -0400

    primer avance

---
+++ container.py
@@ -0,0 +1,39 @@
+from tkinter import *
+import tkinter as tk
+from ventas import Ventas
+from inventario import Inventario
+
+class Container(tk.Frame):
+    def __init__(self, padre, controlador):
+        super().__init__(padre)
+        self.controlador = controlador
+        self.pack()
+        self.place(x=0, y=0, width=600, height=200)
+        self.config(bg="#C6D9E3")
+        self.widgets()
+
+    def show_frames(self, container):
+        top_level = tk.Toplevel(self)
+        frame = container(top_level)
+        frame.config(bg="#C6D9E3")
+        frame.pack(fill="both", expand=True)
+        top_level.geometry("1100x650+120+20")
+        top_level.resizable(False, False)
+
+    def ventas(self):
+        self.show_frames(Ventas)
+
+    def inventario(self):
+        self.show_frames(Inventario)
+
+    def widgets(self):
+
+        frame1 = tk.Frame(self, bg="#C6D9E3")
+        frame1.pack()
+        frame1.place(x=0, y=0, width=800, height=400)
+        
+        btnventas = Button(frame1, bg="green", fg="black", text="Ir a ventas", font=("Arial", 10), command=self.ventas)
+        btnventas.place(x=50, y=30, width=240, height=60)
+
+        btninventario = Button(frame1, bg="blue", fg="white", text="Inventario", font=("Arial", 10), command=self.inventario)
+        btninventario.place(x=50, y=130, width=240, height=60)
--- manager.py
+++ manager.py
@@ -1,2 +1,38 @@
+#mi primer push#
+from container import Container
 from tkinter import Tk, Frame
 
+class Manager(Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Caja registradora version 1.0")
+        self.resizable(False, False)
+        self.configure(bg="#C6D9E3")
+        self.geometry("800x400+120+20")
+
+        self.container = Frame(self, bg="#C6D9E3")
+        self.container.pack(fill="both", expand=True)
+
+        #diccionario#
+        self.frames = {
+            Container: None
+        }
+
+        self.load_frames()
+
+        self.show_frame(Container)
+
+    def load_frames(self):
+        for FrameClass in self.frames.keys():
+            frame = FrameClass(self.container, self)
+            self.frames[FrameClass] = frame
+
+    def show_frame(self, frame_class):
+        frame = self.frames[frame_class]
+        frame.tkraise()
+def main():
+    app = Manager()
+    app.mainloop()
+
+if __name__== "__main__":
+    main()
