ff39b1560e5dbd703abb8563df417b93e7d8f3ee
JonathanKaiKira <jonathancurinan@gmail.com>
2024-08-18 02:20:07 -0400

    añadiendo mas visualización, en el siguiente commit se añadiran funcionalidades para cada boton

--- ventas.py
+++ ventas.py
@@ -6,28 +6,83 @@ class Ventas(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.widgets()
-#1100, 650
+        self.pack(fill=BOTH, expand=True)
 
     def widgets(self):
-        #parametros de visualización frame 1 
+        # Parámetros de visualización frame 1 
         frame1 = tk.Frame(self, bg="#dddddd", highlightbackground="gray", highlightthickness=1)
-        frame1.pack()
         frame1.place(x=0 , y=0, width=1100, height=100)
 
         titulo = tk.Label(self, text="VENTAS", bg="#dddddd", font="sans 30 bold", anchor="center")
-        titulo.pack()
         titulo.place(x=5, y=0, width=1090, height=90)
 
-        #parametros de visualización frame 2
+        # Parámetros de visualización frame 2
         frame2 = tk.Frame(self, bg="#C6D9E3", highlightbackground="gray", highlightthickness=1)
-        frame2.place(x=0,y=100, width=1100, height=550)
+        frame2.place(x=0, y=100, width=1100, height=550)
 
-        lblframe = LabelFrame(frame2, text="Información de la venta",bg="#C6D9E3", font="sans 16 bold")
-        lblframe.place(x=10,y=10,width=1060,height=80)
+        lblframe = LabelFrame(frame2, text="Información de la venta", bg="#C6D9E3", font="sans 16 bold")
+        lblframe.place(x=10, y=10, width=1060, height=80)
 
-        label_numero_factura = tk.Label(lblframe, text="Numero de \nfactura",bg="#C6D9E3", font="sans 12 bold")
-        label_numero_factura.place(x=10,y=5)
+        label_numero_factura = tk.Label(lblframe, text="Numero de \nfactura", bg="#C6D9E3", font="sans 12 bold")
+        label_numero_factura.place(x=10, y=5)
         self.numero_factura = tk.StringVar()
 
         self.entry_numero_factura = ttk.Entry(lblframe, textvariable=self.numero_factura, state="readonly", font="sans 12 bold")
-        self.entry_numero_factura.place(x=100, y=5,width=80)
+        self.entry_numero_factura.place(x=100, y=5, width=80)
+
+        label_nombre = tk.Label(lblframe, text="Productos: ", bg="#C6D9E3", font="sans 12 bold")
+        label_nombre.place(x=190, y=12)
+
+        self.entry_nombre = ttk.Entry(lblframe, font="sans 12 bold")
+        self.entry_nombre.place(x=280, y=10, width=180)
+
+        label_valor = tk.Label(lblframe, text="Precio: ", bg="#C6D9E3", font="sans 12 bold")
+        label_valor.place(x=470, y=12)
+
+        self.entry_valor = ttk.Entry(lblframe, font="sans 12 bold")
+        self.entry_valor.place(x=540, y=10, width=180)
+
+        label_cantidad = tk.Label(lblframe, text="Cantidad: ", bg="#C6D9E3", font="sans 12 bold")
+        label_cantidad.place(x=730, y=12)
+
+        self.entry_cantidad = ttk.Entry(lblframe, font="sans 12 bold")
+        self.entry_cantidad.place(x=820, y=10)
+
+        # Frame para el Treeview
+        treFrame = tk.Frame(frame2, bg="#C6D9E3")
+        treFrame.place(x=150, y=120, width=800, height=200)
+
+        scrol_y = ttk.Scrollbar(treFrame, orient=VERTICAL)
+        scrol_y.pack(side=RIGHT, fill=Y)
+
+        scrol_x = ttk.Scrollbar(treFrame, orient=HORIZONTAL)
+        scrol_x.pack(side=BOTTOM, fill=X)
+
+        # Corregir el problema con `columns` y `xscrollcommand`
+        self.tree = ttk.Treeview(treFrame, columns=("Producto", "Precio", "Cantidad", "Subtotal"), show="headings", height=10, yscrollcommand=scrol_y.set, xscrollcommand=scrol_x.set)
+        scrol_y.config(command=self.tree.yview)
+        scrol_x.config(command=self.tree.xview)
+
+        self.tree.heading("#1", text="Producto")
+        self.tree.heading("#2", text="Precio")
+        self.tree.heading("#3", text="Cantidad")
+        self.tree.heading("#4", text="Subtotal")
+
+        self.tree.column("Producto", anchor="center")
+        self.tree.column("Precio", anchor="center")
+        self.tree.column("Cantidad", anchor="center")
+        self.tree.column("Subtotal", anchor="center")
+
+        self.tree.pack(expand=TRUE, fill=BOTH)
+
+        lblframe1 = LabelFrame(frame2, text="Opciones", bg="#C6D9E3", font="sans 12 bold")
+        lblframe1.place(x=10,y=380,width=1060, height=100)
+
+        boton_agregar = tk.Button(lblframe1, text="Agregar Articulo",bg="#dddddd", font="sans 12 bold")
+        boton_agregar.place(x=50,y=10,width=240,height=50)
+
+        boton_pagar = tk.Button(lblframe1, text="Pagar", bg="#dddddd", font= "sans 12 bold")
+        boton_pagar.place(x=400,y=10,width=240,height=50)
+
+        boton_ver_factura = tk.Button(lblframe1, text="Ver Facturas",bg="#dddddd", font="sans 12 bold")
+        boton_ver_factura.place(x=750,y=10,width=240,height=50)
